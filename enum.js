7aa2283d20823e340d587d1006aea94487d0fbf7
dozcan <dogasava@gmail.com>
2018-08-25 18:18:39 +0300

    enum updates

--- enum.js
+++ enum.js
@@ -1,11 +1,7 @@
 const requestTypeError = {
    
-    account_create  : "account yaratılırken hata oluştu.",
-    contract_deploy : "sözleşme deploy edilirken hata oluştu.",
-    identity        : "blockchain üzerine veri gönderilirken hata oluştu.",
-    identity_transactional : "blockchain üzerine veri gönderirken transaction seviyesinde hata oluştu.",
-    identity_transactional_hash : "blockchain üzerine veri gönderirken transaction seviyesinde hash hatası oluştu.",
-    hash_get        : "blockchain üzerinden veri getirilirken hata oluştu."
+    transactional_error : "infrastructure error",
+    operation_error     : "code base error"
 }
 
 module.exports = requestTypeError;
